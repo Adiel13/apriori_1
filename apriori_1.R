162fcc9d2d92dbafd95656ecfad063c9183d6dba
Kevin Lajpop <klajpop@senacyt.gob.gt>
2024-02-17 12:43:05 -0600

    apriori final

--- apriori_1.R
+++ apriori_1.R
@@ -50,3 +50,48 @@ data2[473, "genero"] <- "female"
 data2[487, "genero"] <- "male"
 
 data2
+
+data3 <- data2[,c(1,2,3,5,6,7,8,11)]
+data3$genero <- ifelse(data3$genero == "male", 1, 0)
+
+data4 <-subset(data3, genero==0)
+data4 <- data4[,-8]
+
+regla <- apriori(data4, parameter = list(support=0.2, confidence =0.5))
+
+inspect(regla[0:100])
+inspect(regla[100:200])
+
+data3
+
+data3$anio_carne<- substr(data3$Reg..Acad., start =1, stop=4)
+
+subset(data3, anio_carne > 8000)
+data3[47, "anio_carne"] <- 1992
+data3[91, "anio_carne"] <- 1986
+data3[92, "anio_carne"] <- 1996
+data3[287, "anio_carne"] <- 1992
+data3[391, "anio_carne"] <- 1983
+data3[392, "anio_carne"] <- 1992
+data3[393, "anio_carne"] <- 1995
+data3[394, "anio_carne"] <- 1996
+
+data3$edad <-as.integer(data3$anio)-as.integer(data3$anio_carne)+18
+
+data3 <- data3[,-8]
+
+data4 <-subset(data3, genero==1)
+data4 <- data4[,-7]
+
+regla <- apriori(data4, parameter = list(support=0.2, confidence =0.5))
+
+inspect(regla[0:100])
+inspect(regla[100:200])
+
+data5 <-subset(data3, semestre==2 & genero==0)
+data5 <-data5[,-2]
+data5 <-data5[,-6]
+
+regla <- apriori(data5, parameter = list(support=0.1, confidence =0.5))
+inspect(regla[0:100])
+inspect(regla[200:300])
