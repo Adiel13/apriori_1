d6d0b254e368fa994f1b93bf18921ef0b39fa2c3
Kevin Lajpop <klajpop@senacyt.gob.gt>
2024-02-17 11:00:29 -0600

    primer algoritmo apriori

--- apriori_1.R
+++ apriori_1.R
@@ -4,3 +4,13 @@ install.packages("genero")
 library(genero)
 
 data = read.csv('C:/Users/kevin/OneDrive/Documentos/data.csv', sep=";")
+data[,c(3,4)]
+subset(data, Lab. >60)
+subset(data, Lab. >60)[,c(3,4)]
+
+data2 <- data[,c(1,2,3,4,8,9,10,11)]
+
+regla <- apriori(data2, parameter = list(support=0.2, confidence =0.5))
+inspect(regla[0:100])
+inspect(regla[100:200])
+
